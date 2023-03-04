43159bb53106fc83793a0b321bfdd72c34aab906
wildmolasses <changes@gmail.com>
2023-03-04 15:20:20 -0700

    chore: init from https://github.com/xBalbinus/fevm-foundry-kit at f287165

---
+++ script/SimpleCoin.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.11;
+
+import "@forge-std/Script.sol";
+import "../src/SimpleCoin.sol";
+
+contract MyScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        SimpleCoin coin = new SimpleCoin();
+
+        vm.stopBroadcast();
+    }
+}
